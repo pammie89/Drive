ff9eb7c73f76843874325e8862b26857d387f6ad
Pamela Afaneh <pamela.afaneh@gmail.com>
2023-01-07 18:43:48 -0600

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Footer.module.css";
+import Logo from "../../images/logo_dark.png";
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.container}>
+        <img src={Logo} alt="/" />
+        <button>Share a Car</button>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
